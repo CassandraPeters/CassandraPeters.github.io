5a5301ca5ca03292f9fe714fd9ed4dba4b0a66a3
GraemePeters <Graeme.B.Peters@gmail.com>
2017-04-11 14:10:37 -0500

    Fiddled with adding strength to recipes

--- itemCreator.js
+++ itemCreator.js
@@ -1,6 +1,41 @@
+var SecondLevel = {
+	AttackUp:5,
+	DefenseUp:5,
+	MovingSpeed:5,
+	Fireproof:7,
+	Quietness:6,
+	ResistCold:6,
+	ResistHot:6,
+	ResistElectric:4
+};
+var ThirdLevel = {
+	AttackUp:7,
+	DefenseUp:7,
+	MovingSpeed:7,
+	Fireproof:0,
+	Quietness:9,
+	ResistCold:0,
+	ResistHot:0,
+	ResistElectric:6
+};
+var countEffects = ["ExGutsMaxUp","GutsRecover","LifeMaxUp"];
+var maxStrength2 = ["Fireproof","ResistCold","ResistHot"];
+var maxStrength3 = ["AttackUp","DefenseUp","MovingSpeed","Quietness","ResistElectric"];
+
+function getStrength(recipe)
+{
+	var effectType = recipe["effect"];
+	var level = recipe["level"];
+	var strength = 1;
+	
+	if (!maxStrength2.includes(effectType) && level > ThirdLevel[effectType]) strength = 3;
+	else if (level > SecondLevel[effectType]) strength = 2;
+	
+	return strength;
+}
+
 function createItemHTML(item, itemType, div)
 {
-
 	var minutes = Math.floor(item["duration"] / 60);
 	var seconds = item["duration"] % 60;
 	if (minutes < 10) minutes = "0" + minutes;
@@ -8,7 +43,11 @@ function createItemHTML(item, itemType, div)
 	
 	var itemE = "<div class=\"itemEffects specialEffect\">\n" +
 	"<div class=\"effectIcon\"><img src=\"images/"+item["effect"]+".png\">\n";
-	if (item["level"] > 0) itemE += 	"<p class=\"effectNumber\">"+item["level"]+"</p>\n";
+	if (countEffects.includes(item["effect"]) || (item["level"] > 0 && itemType == "ing")) itemE += 	"<p class=\"effectNumber\">"+item["level"]+"</p>\n";
+	else if (itemType == "rec")
+	{
+		var strength = getStrength(item);
+	}
 	itemE += "</div></div>\n";
 	
 	var toWrite = "";
